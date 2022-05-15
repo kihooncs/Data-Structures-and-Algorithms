e5ff92d0d8d52a04bd89ca4449c8a874ba8862ef
Kihoon Lee <65799897+kihooncs@users.noreply.github.com>
2022-05-15 14:30:41 -0700

    Time: 0 ms (100.00%), Space: 41.4 MB (12.70%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,19 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        // base cases
-        if(n <= 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-    
-        int one_step_before = 2;
-        int two_steps_before = 1;
-        int all_ways = 0;
-    
-        for(int i=2; i<n; i++){
-    	    all_ways = one_step_before + two_steps_before;
-    	    two_steps_before = one_step_before;
-            one_step_before = all_ways;
+        int one = 1;
+        int two = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int temp = one + two;
+            two = one;
+            one = temp;
         }
-        return all_ways;
+        return one;
     }
 }
